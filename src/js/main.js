d4a09d747a0dd51c75d17e413525ce41bc642068
sole <sole@mozilla.com>
2014-08-12 05:40:20 -0700

    points seem to be in the right place!

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,11 @@ var contentWidth = 320,
 	contentHeight = 240;
 
 var renderer = new THREE.WebGLRenderer({ antialias: true, preserveDrawingBuffer: true });
+renderer.setClearColor(new THREE.Color(0x000000), 1.0);
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(60, contentWidth / contentHeight, 1, 100000);
+var cameraTarget = new THREE.Vector3(0, 0, 0);
+var cameraPosition = new THREE.Vector3(0, 0, 100);
 
 
 document.body.appendChild(renderer.domElement);
@@ -14,9 +17,28 @@ document.body.appendChild(renderer.domElement);
 window.addEventListener('resize', onWindowResized);
 onWindowResized();
 
-var triangles = tesselate([0, 0], [100, 100]);
+var triangles = tesselate([0, 0], [100, 100], 5, 5);
 
 console.log(triangles);
+
+var geometry = new THREE.Geometry();
+triangles.forEach(function(t) {
+	t.forEach(function(tt) {
+		var v = new THREE.Vector3(tt[0], tt[1], 0);
+		geometry.vertices.push(v);
+		console.log('boo', v.x, v.y);
+	});
+});
+var material = new THREE.PointCloudMaterial({ color: 0xFFFF00, size: 5 });
+var mesh = new THREE.PointCloud(geometry, material);
+//mesh.rotation.y = Math.PI / 4;
+scene.add(mesh);
+console.log(material);
+
+var debugCube = new THREE.Mesh( new THREE.BoxGeometry(10, 10, 10), new THREE.MeshBasicMaterial({ color: 0x00FF00, wireframe: true }) );
+debugCube.scale.multiplyScalar(10);
+scene.add(debugCube);
+requestAnimationFrame(render);
 
 function onWindowResized() {
 	contentWidth = window.innerWidth;
@@ -26,4 +48,14 @@ function onWindowResized() {
 	camera.updateProjectionMatrix();
 }
 
-renderer.render(scene, camera);
+function render(time) {
+	//requestAnimationFrame(render);
+	
+	var t = time * 0.0001;
+	var r = 200;
+	camera.position.set( r * Math.sin(t), 0, r * Math.cos(t));
+	//camera.position.copy(cameraPosition);
+	camera.lookAt(cameraTarget);
+	renderer.clear();
+	renderer.render(scene, camera);
+}
--- src/js/tesselate.js
+++ src/js/tesselate.js
@@ -5,11 +5,37 @@ function tesselate(pointA, pointB, subdivisionsX, subdivisionsY) {
 // |                             |
 // +--------------------- B(x2,y2)
 
+	var vertices = [];
+	
 	subdivisionsX = subdivisionsX !== undefined ? subdivisionsX : 3;
 	subdivisionsY = subdivisionsY !== undefined ? subdivisionsY : 3;
 
-	
+	// 1) generate the vertices in a 'grid' fashion
+	var dx = pointB[0] - pointA[0];
+	var dy = pointB[1] - pointA[1];
+	var incX = dx / subdivisionsX;
+	var incY = dy / subdivisionsY;
 
+	console.log(dx, dy, incX, incY);
+
+	var y = pointA[1];
+
+	for(var j = 0; j < subdivisionsY; j++) {
+		var row = [];
+		var x = pointA[0];
+
+		for(var i = 0; i < subdivisionsX; i++) {
+			console.log(i, j, x, y);
+			row.push([x, y]);
+			x += incX;
+		}
+		
+		vertices.push(row);
+		y += incY;
+
+	}
+
+	return vertices;
 }
 
 module.exports = tesselate;
